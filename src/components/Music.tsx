5784cd654d12d2e49b4d693bdc4abba9d6b2cd77
Matt Kenney <mkenney@mjk-tech.com>
2025-04-15 20:16:52 -0400

    update Music artist names; upgrade deps

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -8,11 +8,11 @@ export function Music() {
       <Panel.Block>
         <Content>
           <Link to="/music/JmnZHQNN5cc">Caroline Polachek</Link><br/>
-          <Link to="/music/DZ8Ue8lNGIE">Christian Scott Quintet</Link><br/>
-          <Link to="/music/1ox4_e1PIfM">Christine and the Queens</Link><br/>
+          <Link to="/music/DZ8Ue8lNGIE">Chief Adjuah/Christian Scott Quintet</Link><br/>
           <Link to="/music/qfkEK6MeD0U">Horsegirl</Link><br/>
           <Link to="/music/i6-SwIM7UEc">Makaya McCraven</Link><br/>
-          <Link to="/music/Od9I3P06_fQ">Mother Falcon</Link>
+          <Link to="/music/Od9I3P06_fQ">Mother Falcon</Link><br/>
+          <Link to="/music/1ox4_e1PIfM">Rahim Redcar/Christine and the Queens</Link>
         </Content>
       </Panel.Block>
     </Panel>

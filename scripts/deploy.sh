390a49a828e54b8f0e7997b5471a84060808511a
Matt Kenney <mkenney@mjk-tech.com>
2026-04-07 08:25:34 -0400

    `npm audit fix`

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+npm run build
+. ./aws.local
+cd dist
+aws s3 cp --recursive . "s3://${BUCKET}"
+aws cloudfront create-invalidation \
+  --distribution-id "${DISTRIBUTION_ID}" \
+  --no-cli-pager \
+  --paths / /index.html
